3433c64a8aa795fa8351c482f4dea19fe0f87ea2
Fr_Dae <dae.anthony@gmail.com>
2026-02-10 22:46:48 +0100

    1.5.6 for  factorio v2.0
        - Updated mod to be compatible with factorio 2.0
        - added additional green color for turbo transport belts

--- data-updates.lua
+++ data-updates.lua
@@ -1,51 +1,136 @@
--- changes color of base belts
-data.raw["transport-belt"]["transport-belt"].friendly_map_color = {r = 0.98, g = 0.73, b = 0.0} -- 250, 186, 0
-data.raw["splitter"]["splitter"].friendly_map_color = {r = 0.78, g = 0.58, b = 0.0} -- 200, 149, 0
-data.raw["underground-belt"]["underground-belt"].friendly_map_color = {r = 0.74, g = 0.55, b = 0.0} -- 188, 140, 0
+local beltColorData = {
+  --base
+  ["transport-belt"] = {r = 0.98, g = 0.73, b = 0.0}, -- 250, 186, 0
+  ["fast-transport-belt"] = {r = 0.98, g = 0.27, b = 0.06}, -- 250, 69, 15
+  ["express-transport-belt"] = {r = 0.15, g = 0.67, b = 0.71}, -- 38, 171, 181
+  --K2
+  ["kr-advanced-transport-belt"] = {r = 0.13, g = 0.92, b = 0.09}, -- 34, 235, 23
+  ["kr-superior-transport-belt"] = {r = 0.82, g = 0.004, b = 0.97}, -- 210, 1, 247
+  --5Dim
+  ["5d-transport-belt-04"] = {r = 1, g = 0.63, b = 0.97}, -- 255, 160, 247
+  ["5d-transport-belt-05"] = {r = 0.18, g = 0.75, b = 0.16}, -- 47, 190, 40
+  ["5d-transport-belt-06"] = {r = 0.42, g = 0.28, b = 0.18}, -- 107, 71, 478
+  ["5d-transport-belt-07"] = {r = 0.45, g = 0.24, b = 0.61}, -- 116, 60, 155
+  ["5d-transport-belt-08"] = {r = 0.87, g = 0.87, b = 0.87}, -- 222, 222, 222
+  ["5d-transport-belt-09"] = {r = 0.98, g = 0.49, b = 0.06}, -- 250, 126, 16
+  ["5d-transport-belt-10"] = {r = 0.44, g = 0.45, b = 1}, -- 111, 114, 255
+  --Bob's
+  ["basic-transport-belt"] = {r = 0.46, g = 0.47, b = 0.47}, -- 118, 120, 120
+  ["turbo-transport-belt"] = {r = 0.97, g = 0.07, b = 1.0}, -- 247, 18, 255
+  ["ultimate-transport-belt"] = {r = 0.07, g = 1.0, b = 0.62}, -- 18, 255, 158
+  --RandomFactorioThings/PlutoniumEnergy
+  ["nuclear-transport-belt"] = {r = 0.0, g = 1, b = 0.0}, -- 0, 255, 0
+  ["plutonium-transport-belt"] = {r = 0.0, g = 1, b = 0.91} -- 0, 255, 231
+}
 
-data.raw["transport-belt"]["fast-transport-belt"].friendly_map_color = {r = 0.98, g = 0.27, b = 0.06} -- 250, 69, 15
-data.raw["splitter"]["fast-splitter"].friendly_map_color = {r = 0.78, g = 0.22, b = 0.05} -- 200, 55, 12
-data.raw["underground-belt"]["fast-underground-belt"].friendly_map_color = {r = 0.74, g = 0.20, b = 0.04} -- 188, 52, 11
+local function changeColor(belt, percent)
+  return {belt.r * percent, belt.g * percent, belt.b * percent}
+end
 
-data.raw["transport-belt"]["express-transport-belt"].friendly_map_color = {r = 0.15, g = 0.67, b = 0.71} -- 38, 171, 181
-data.raw["splitter"]["express-splitter"].friendly_map_color = {r = 0.12, g = 0.54, b = 0.57} -- 30, 137, 145
-data.raw["underground-belt"]["express-underground-belt"].friendly_map_color = {r = 0.11, g = 0.50, b = 0.53} -- 29, 128, 136
+local loaderColorData = {
+  ["loader"] = changeColor(beltColorData["transport-belt"], .90),
+  ["fast-loader"] = changeColor(beltColorData["fast-transport-belt"], .90),
+  ["express-loader"] = changeColor(beltColorData["express-transport-belt"], .90),
+  ["5d-loader-04"] = changeColor(beltColorData["5d-transport-belt-04"], .90),
+  ["5d-loader-05"] = changeColor(beltColorData["5d-transport-belt-05"], .90),
+  ["5d-loader-06"] = changeColor(beltColorData["5d-transport-belt-06"], .90),
+  ["5d-loader-07"] = changeColor(beltColorData["5d-transport-belt-07"], .90),
+  ["5d-loader-08"] = changeColor(beltColorData["5d-transport-belt-08"], .90)
+}
 
-data.raw["transport-belt"]["turbo-transport-belt"].friendly_map_color = {r = 0.49, g = 0.58, b = 0.20} -- 125, 148, 51
-data.raw["splitter"]["turbo-splitter"].friendly_map_color = {r = 0.42, g = 0.55, b = 0.15} -- 103, 140, 38
-data.raw["underground-belt"]["turbo-underground-belt"].friendly_map_color = {r = 0.35, g = 0.52, b = 0.10} -- 89, 132, 25
+local splitterColorData = { --80%
+  ["splitter"] = changeColor(beltColorData["transport-belt"], .80),
+  ["fast-splitter"] = changeColor(beltColorData["fast-transport-belt"], .80),
+  ["express-splitter"] = changeColor(beltColorData["express-transport-belt"], .80),
+  ["kr-advanced-splitter"] = changeColor(beltColorData["kr-advanced-transport-belt"], .80),
+  ["kr-superior-splitter"] = changeColor(beltColorData["kr-superior-transport-belt"], .80),
+  ["5d-splitter-04"] = changeColor(beltColorData["5d-transport-belt-04"], .80),
+  ["5d-splitter-05"] = changeColor(beltColorData["5d-transport-belt-05"], .80),
+  ["5d-splitter-06"] = changeColor(beltColorData["5d-transport-belt-06"], .80),
+  ["5d-splitter-07"] = changeColor(beltColorData["5d-transport-belt-07"], .80),
+  ["5d-splitter-08"] = changeColor(beltColorData["5d-transport-belt-08"], .80),
+  ["5d-splitter-09"] = changeColor(beltColorData["5d-transport-belt-09"], .80),
+  ["5d-splitter-10"] = changeColor(beltColorData["5d-transport-belt-10"], .80),
+  ["basic-splitter"] = changeColor(beltColorData["basic-transport-belt"], .80),
+  ["turbo-splitter"] = changeColor(beltColorData["turbo-transport-belt"], .80),
+  ["ultimate-splitter"] = changeColor(beltColorData["ultimate-transport-belt"], .80),
+  ["nuclear-splitter"] = changeColor(beltColorData["ultimate-transport-belt"], .80),
+  ["plutonium-splitter"] = changeColor(beltColorData["plutonium-transport-belt"], .80),
+}
 
---changes color of pipes/storage tank
--- data.raw["pipe"]["pipe"].friendly_map_color = {r = 0.29, g = 0.09, b = 0.56} -- 74, 23, 143
--- data.raw["pipe-to-ground"]["pipe-to-ground"].friendly_map_color = {r = 0.29, g = 0.09, b = 0.56}
--- data.raw["storage-tank"]["storage-tank"].friendly_map_color = {r = 0.29, g = 0.09, b = 0.56}
+local undergroundColorData = { --75%
+  ["underground-belt"] = changeColor(beltColorData["transport-belt"], .70),
+  ["fast-underground-belt"] = changeColor(beltColorData["fast-transport-belt"], .70),
+  ["express-underground-belt"] = changeColor(beltColorData["express-transport-belt"], .70),
+  ["kr-advanced-underground-belt"] = changeColor(beltColorData["kr-advanced-transport-belt"], .70),
+  ["kr-superior-underground-belt"] = changeColor(beltColorData["kr-superior-transport-belt"], .70),
+  ["5d-underground-belt-30-01"] = changeColor(beltColorData["transport-belt"], .70),
+  ["5d-underground-belt-50-01"] = changeColor(beltColorData["transport-belt"], .70),
+  ["5d-fast-underground-belt-30-02"] = changeColor(beltColorData["fast-transport-belt"], .70),
+  ["5d-fast-underground-belt-50-02"] = changeColor(beltColorData["fast-transport-belt"], .70),
+  ["5d-express-underground-belt-30-03"] = changeColor(beltColorData["express-transport-belt"], .70),
+  ["5d-express-underground-belt-50-03"] = changeColor(beltColorData["express-transport-belt"], .70),
+  ["5d-underground-belt-04"] = changeColor(beltColorData["5d-transport-belt-04"], .70),
+  ["5d-underground-belt-30-04"] = changeColor(beltColorData["5d-transport-belt-04"], .70),
+  ["5d-underground-belt-50-04"] = changeColor(beltColorData["5d-transport-belt-04"], .70),
+  ["5d-underground-belt-05"] = changeColor(beltColorData["5d-transport-belt-05"], .70),
+  ["5d-underground-belt-30-05"] = changeColor(beltColorData["5d-transport-belt-05"], .70),
+  ["5d-underground-belt-50-05"] = changeColor(beltColorData["5d-transport-belt-05"], .70),
+  ["5d-underground-belt-06"] = changeColor(beltColorData["5d-transport-belt-06"], .70),
+  ["5d-underground-belt-30-06"] = changeColor(beltColorData["5d-transport-belt-06"], .70),
+  ["5d-underground-belt-50-06"] = changeColor(beltColorData["5d-transport-belt-06"], .70),
+  ["5d-underground-belt-07"] = changeColor(beltColorData["5d-transport-belt-07"], .70),
+  ["5d-underground-belt-30-07"] = changeColor(beltColorData["5d-transport-belt-07"], .70),
+  ["5d-underground-belt-50-07"] = changeColor(beltColorData["5d-transport-belt-07"], .70),
+  ["5d-underground-belt-08"] = changeColor(beltColorData["5d-transport-belt-08"], .70),
+  ["5d-underground-belt-30-08"] = changeColor(beltColorData["5d-transport-belt-08"], .70),
+  ["5d-underground-belt-50-08"] = changeColor(beltColorData["5d-transport-belt-08"], .70),
+  ["5d-underground-belt-09"] = changeColor(beltColorData["5d-transport-belt-09"], .70),
+  ["5d-underground-belt-30-09"] = changeColor(beltColorData["5d-transport-belt-09"], .70),
+  ["5d-underground-belt-50-09"] = changeColor(beltColorData["5d-transport-belt-09"], .70),
+  ["5d-underground-belt-10"] = changeColor(beltColorData["5d-transport-belt-10"], .70),
+  ["5d-underground-belt-30-10"] = changeColor(beltColorData["5d-transport-belt-10"], .70),
+  ["5d-underground-belt-50-10"] = changeColor(beltColorData["5d-transport-belt-10"], .70),
+  ["basic-underground-belt"] = changeColor(beltColorData["basic-transport-belt"], .70),
+  ["turbo-underground-belt"] = changeColor(beltColorData["turbo-transport-belt"], .70),
+  ["ultimate-underground-belt"] = changeColor(beltColorData["ultimate-transport-belt"], .70),
+  ["nuclear-underground-belt"] = changeColor(beltColorData["ultimate-transport-belt"], .70),
+  ["plutonium-underground-belt"] = changeColor(beltColorData["plutonium-transport-belt"], .70)
+}
 
--- data.raw["heat-pipe"]["heat-pipe"].friendly_map_color = {r = 0.56, g = 0.0, b = 0.0} -- 142, 0, 0
--- data.raw["reactor"]["nuclear-reactor"].friendly_map_color = {r = 0.16, g = 0.73, b = 0.15} -- 41, 186, 37
-
---log("EMC mod settings test: "..tostring(settings.startup["Use-Mod-Color-for-pipes"].value))
+for k,v in pairs(data.raw["transport-belt"]) do
+  if beltColorData[k] then
+    data.raw["transport-belt"][k].friendly_map_color = beltColorData[k]
+  end
+end
+for k,v in pairs(data.raw["splitter"]) do
+  if splitterColorData[k] then
+    data.raw["splitter"][k].friendly_map_color = splitterColorData[k]
+  end
+end
+for k,v in pairs(data.raw["underground-belt"]) do
+  if undergroundColorData[k] then
+    data.raw["underground-belt"][k].friendly_map_color = undergroundColorData[k]
+  end
+end
+for k,v in pairs(data.raw.loader) do
+  if loaderColorData[k] then
+    data.raw.loader[k].friendly_map_color = loaderColorData[k]
+  end
+end
 
 if settings.startup["Use-Mod-Color-for-pipes"].value then
 	for _, v in pairs(data.raw["pipe"]) do
 		v.friendly_map_color = {r = 0.35, g = 0.15, b = 0.62} -- 89, 38, 158
 	end
-end
-
-if settings.startup["Use-Mod-Color-for-pipes"].value then
-	for _, v in pairs(data.raw["pipe-to-ground"]) do
-		v.friendly_map_color = {r = 0.35, g = 0.15, b = 0.62} -- 89, 38, 158
+  for _, v in pairs(data.raw["storage-tank"]) do
+		v.friendly_map_color = {r = 0.22, g = 0.07, b = 0.45} -- 56, 18, 115
 	end
-end
-
-if settings.startup["Use-Mod-Color-for-pipes"].value then
 	for _, v in pairs(data.raw["pump"]) do
 		v.friendly_map_color = {r = 0.20, g = 0.05, b = 0.40} -- 51, 13, 102
 	end
-end
-
-if settings.startup["Use-Mod-Color-for-pipes"].value then
-	for _, v in pairs(data.raw["storage-tank"]) do
-		v.friendly_map_color = {r = 0.22, g = 0.07, b = 0.45} -- 56, 18, 115
+	for _, v in pairs(data.raw["pipe-to-ground"]) do
+		v.friendly_map_color = {r = 0.35, g = 0.15, b = 0.62} -- 89, 38, 158
 	end
 end
 
@@ -61,59 +146,6 @@ end
 
 --data.raw["mining-drill"]["burner-mining-drill"].map_color = {r = 0.0, g = 0.37, b = 0.08} -- 0, 95, 20
 --data.raw["mining-drill"]["electric-mining-drill"].map_color = {r = 0.0, g = 0.37, b = 0.08} -- 74, 23, 143
---Bob's Belts
-if data.raw["transport-belt"]["ultimate-transport-belt"] ~= nil then --green
-	data.raw["transport-belt"]["ultimate-transport-belt"].friendly_map_color = {r = 0.07, g = 1.0, b = 0.62} -- 18, 255, 158
-	data.raw["splitter"]["ultimate-splitter"].friendly_map_color = {r = 0.06, g = 0.80, b = 0.50} -- 14, 205, 126
-	data.raw["underground-belt"]["ultimate-underground-belt"].friendly_map_color = {r = 0.05, g = 0.75, b = 0.46} -- 14, 191, 119
-end
-
-if data.raw["transport-belt"]["turbo-transport-belt"] ~= nil and data.raw["transport-belt"]["ultimate-transport-belt"] ~= nil then --purple
-	data.raw["transport-belt"]["turbo-transport-belt"].friendly_map_color = {r = 0.97, g = 0.07, b = 1.0} -- 247, 18, 255
-	data.raw["splitter"]["turbo-splitter"].friendly_map_color = {r = 0.77, g = 0.06, b = 0.80} -- 198, 14, 204
-	data.raw["underground-belt"]["turbo-underground-belt"].friendly_map_color = {r = 0.73, g = 0.05, b = 0.75} -- 185, 14, 191
-end
-
---5dim belts
-if data.raw["underground-belt"]["5d-mk1-transport-belt-to-ground-30"] then
-	data.raw["underground-belt"]["5d-mk1-transport-belt-to-ground-30"].friendly_map_color = {r = 0.74, g = 0.55, b = 0.0} -- 188, 140, 0
-	data.raw["underground-belt"]["5d-mk2-transport-belt-to-ground-30"].friendly_map_color = {r = 0.74, g = 0.20, b = 0.04} -- 188, 52, 11
-	data.raw["underground-belt"]["5d-mk3-transport-belt-to-ground-30"].friendly_map_color = {r = 0.11, g = 0.50, b = 0.53} -- 29, 128, 136
-	
-	data.raw["underground-belt"]["5d-mk1-transport-belt-to-ground-50"].friendly_map_color = {r = 0.74, g = 0.55, b = 0.0} -- 188, 140, 0
-	data.raw["underground-belt"]["5d-mk2-transport-belt-to-ground-50"].friendly_map_color = {r = 0.74, g = 0.20, b = 0.04} -- 188, 52, 11
-	data.raw["underground-belt"]["5d-mk3-transport-belt-to-ground-50"].friendly_map_color = {r = 0.11, g = 0.50, b = 0.53} -- 29, 128, 136
-end
-
-if data.raw["transport-belt"]["5d-mk4-transport-belt"] ~= nil then
-	data.raw["transport-belt"]["5d-mk4-transport-belt"].friendly_map_color = {r = 0.08, g = 0.66, b = 0.14} -- 20, 168, 36
-	data.raw["splitter"]["5d-mk4-splitter"].friendly_map_color = {r = 0.06, g = 0.53, b = 0.11} -- 16, 134, 29
-	
-	data.raw["underground-belt"]["5d-mk4-transport-belt-to-ground"].friendly_map_color = {r = 0.06, g = 0.49, b = 0.11} -- 15, 126, 27
-	data.raw["underground-belt"]["5d-mk4-transport-belt-to-ground-30"].friendly_map_color = {r = 0.06, g = 0.49, b = 0.11} -- 15, 126, 27
-	data.raw["underground-belt"]["5d-mk4-transport-belt-to-ground-50"].friendly_map_color = {r = 0.06, g = 0.49, b = 0.11} -- 15, 126, 27
-end
-
-if data.raw["transport-belt"]["5d-mk5-transport-belt"] ~= nil then
-	data.raw["transport-belt"]["5d-mk5-transport-belt"].friendly_map_color = {r = 0.89, g = 0.91, b = 0.96} -- 227, 232, 245
-	data.raw["splitter"]["5d-mk5-splitter"].friendly_map_color = {r = 0.71, g = 0.73, b = 0.77} -- 182, 186, 196
-	
-	data.raw["underground-belt"]["5d-mk5-transport-belt-to-ground"].friendly_map_color = {r = 0.67, g = 0.68, b = 0.72} -- 170, 174, 184
-	data.raw["underground-belt"]["5d-mk5-transport-belt-to-ground-30"].friendly_map_color = {r = 0.67, g = 0.68, b = 0.72} -- 170, 174, 184
-	data.raw["underground-belt"]["5d-mk5-transport-belt-to-ground-50"].friendly_map_color = {r = 0.67, g = 0.68, b = 0.72} -- 170, 174, 184
-end
-
---[[
---Better Belts mods
-if data.raw["transport-belt"][""] ~= nil then
-	data.raw["transport-belt"][""].map_color = {r = 0.14, g = 0.94, b = 0.05} --36, 240, 13
-	data.raw["transport-belt"][""].map_color
-	data.raw["transport-belt"][""].map_color
-	data.raw["transport-belt"][""].map_color
-	data.raw["transport-belt"][""].map_color
-	
-end
---]]
 
 --Electric Poles including those in Bob's mods and 5dim
 for _, v in pairs(data.raw["electric-pole"]) do
@@ -145,7 +177,7 @@ if settings.startup["Use-Mod-Color-for-roboports"].value then
 	end
 end
 
--- local n = 1
+
 
 
 data.raw["unit-spawner"]["biter-spawner"].enemy_map_color = {r = 1.0, g = 0.10, b = 0.10} -- 255, 25, 25
@@ -155,7 +187,6 @@ data.raw["unit-spawner"]["spitter-spawner"].enemy_map_color = {r = 0.76, g = 0.1
 --spitters
 
 for k,v in pairs(data.raw["unit"]) do
-log("k is "..k)
 	if k:find("biter") then
 			v.enemy_map_color = {r = 1.0, g = 0.33, b = 0.22} -- 255, 85, 55
 	elseif k:find("spitter") then
